03470084c532ad807fa84b2ae72dfa43e11b9b62
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-08-20 12:59:07 -0400

    add data storage impl for model request and model response

--- src/main/java/io/ohmvir/plugins/jenkinscr/api/client/ModelRequest.java
+++ src/main/java/io/ohmvir/plugins/jenkinscr/api/client/ModelRequest.java
@@ -1,27 +1,65 @@
 package io.ohmvir.plugins.jenkinscr.api.client;
 
+import io.ohmvir.plugins.jenkinscr.api.models.ModelInputType;
 import io.ohmvir.plugins.jenkinscr.api.models.ModelOutputType;
 import io.ohmvir.plugins.jenkinscr.configuration.agents.AgentConfiguration;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.UUID;
+import java.util.*;
 
-public abstract class ModelRequest implements Cloneable {
+public class ModelRequest implements Cloneable {
     private final AgentConfiguration agentConfiguration;
+    private final HashMap<UUID, byte[]> files = new HashMap<>();
+    private final HashMap<UUID, ModelInputType> fileInputTypes = new HashMap<>();
+    private @Getter @Setter String promptText = "";
+    private @Getter @Setter ModelConversation conversationHistory = null;
+    private final Set<ModelOutputType> requestedOutputTypes = new HashSet<>();
+    private final Set<ModelInputType> inputTypes = new HashSet<>();
+
     public ModelRequest(AgentConfiguration agentConfiguration){
         this.agentConfiguration = agentConfiguration;
     }
-    public abstract UUID AttachFile(byte[] fileBytes);
-    public abstract void RemoveFile(UUID fileId);
-    public abstract UUID[] GetFiles();
+
+    public UUID AttachFile(byte[] fileBytes, ModelInputType fileType){
+        UUID uuid = UUID.randomUUID();
+        files.put(uuid, fileBytes);
+        inputTypes.add(fileType);
+        fileInputTypes.put(uuid, fileType);
+        return uuid;
+    }
+    public boolean RemoveFile(UUID fileId){
+        if(fileInputTypes.containsKey(fileId) && fileInputTypes.values().stream().filter(i -> i.equals(fileInputTypes.get(fileId))).count() > 1){
+            inputTypes.remove(fileInputTypes.get(fileId));
+        }
+        fileInputTypes.remove(fileId);
+        return files.remove(fileId) != null;
+    }
+    public Set<UUID> GetFileIds(){
+        return files.keySet();
+    }
     /** Appends text to the prompt. */
-    public abstract void AttachText(String text);
-    /** Gets the current prompt text. Does not include the system prompt */
-    public abstract String GetText();
-    /** Sets the full prompt text */
-    public abstract void SetText(String newPrompt);
-    /** Attaches the conversation history to the request */
-    public abstract void AddConversationHistory(ModelConversation conversation);
-    public abstract void RequestOutputType(ModelOutputType outputType);
+    public void AttachText(String text){
+        if(!text.isEmpty()){
+            inputTypes.add(ModelInputType.TEXT);
+        }
+        promptText += text;
+    }
+
+    public Set<ModelInputType> getInputTypes(){
+        return Collections.unmodifiableSet(inputTypes);
+    }
+
+    public Set<ModelOutputType> getOutputTypes(){
+        return Collections.unmodifiableSet(requestedOutputTypes);
+    }
+
+    public void RequestOutputType(ModelOutputType outputType){
+        requestedOutputTypes.add(outputType);
+    }
+    public void RemoveOutputTypeRequest(ModelOutputType outputType){
+        requestedOutputTypes.remove(outputType);
+    }
   //  public abstract void AttachTool(ToolData tool); future TODO
   //  public abstract void AttachSkill(SkillData skill); future TODO
 
@@ -31,5 +69,14 @@ public abstract class ModelRequest implements Cloneable {
      * @return A shallow clone of the prompt. File byte data will be copied, but Skills and tool data which are stored in classes will continue to refer to the old data.
      */
     @Override
-    public abstract ModelRequest clone();
+    public ModelRequest clone() {
+        ModelRequest newRequest = new ModelRequest(agentConfiguration);
+        newRequest.files.putAll(files);
+        newRequest.inputTypes.addAll(inputTypes);
+        newRequest.fileInputTypes.putAll(fileInputTypes);
+        newRequest.promptText = promptText;
+        newRequest.conversationHistory = conversationHistory;
+        newRequest.requestedOutputTypes.addAll(requestedOutputTypes);
+        return newRequest;
+    }
 }
--- src/main/java/io/ohmvir/plugins/jenkinscr/api/client/ModelResponse.java
+++ src/main/java/io/ohmvir/plugins/jenkinscr/api/client/ModelResponse.java
@@ -1,4 +1,46 @@
 package io.ohmvir.plugins.jenkinscr.api.client;
 
-public abstract class ModelResponse {
+import io.ohmvir.plugins.jenkinscr.api.models.ModelOutputType;
+import lombok.Getter;
+
+import java.util.*;
+
+public class ModelResponse {
+    private HashMap<UUID, byte[]> fileOutputs = new HashMap<>();
+    private HashMap<UUID, ModelOutputType> fileOutputTypes = new HashMap<>();
+    private HashSet<ModelOutputType> outputTypes = new HashSet<>();
+    private @Getter String responseText = "";
+    // TODO add tool calls and other model outputs here.
+
+    public ModelResponse(HashMap<UUID, byte[]> fileOutputs, HashMap<UUID, ModelOutputType> fileOutputTypes,
+                         HashSet<ModelOutputType> outputTypes, String responseText) {
+        this.fileOutputs = fileOutputs;
+        this.fileOutputTypes = fileOutputTypes;
+        this.outputTypes = outputTypes;
+        this.responseText = responseText;
+    }
+
+    public void AddOutputType(ModelOutputType outputType){
+        outputTypes.add(outputType);
+    }
+
+    public ModelOutputType GetFileOutputType(UUID fileId){
+        return fileOutputTypes.get(fileId);
+    }
+
+    public void AddFileOutput(UUID outputFileId, byte[] fileOutputData){
+        fileOutputs.put(outputFileId, fileOutputData);
+    }
+
+    public Map<UUID, byte[]> getFileOutputs() {
+        return Collections.unmodifiableMap(fileOutputs);
+    }
+
+    public boolean HasOutputType(ModelOutputType outputType){
+        return outputTypes.contains(outputType);
+    }
+
+    public void PushToConversation(ModelConversation conversation){
+        conversation.AddResponse(this);
+    }
 }
