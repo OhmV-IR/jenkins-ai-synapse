7ef63075bf70cdd35b1a98a3abdeea38cbcf464b
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-01 23:38:11 -0700

    Remove old unimplemented config

--- src/test/java/io/ohmvir/plugins/jenkinsaisynapse/DescriptorRegistrationTest.java
+++ src/test/java/io/ohmvir/plugins/jenkinsaisynapse/DescriptorRegistrationTest.java
@@ -26,8 +26,8 @@ public class DescriptorRegistrationTest {
             FullPrompt.class,
             BasicSkillConfiguration.class,
             BasicSkillFileConfiguration.class,
-            DeclaredSkillConfiguration.class,
-            GithubFolderSkillConfiguration.class);
+            DeclaredSkillConfiguration.class
+    );
 
     // 3. Classes that extend Descriptor directly.
     private static final List<Class<? extends Describable<?>>> STANDALONE_CONCRETE_CLASSES_TO_TEST = List.of();
