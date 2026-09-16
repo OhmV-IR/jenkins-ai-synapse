4a6c9ed84eaeed58eb66ba42a924f3c72ec0a61b
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-15 19:42:06 -0700

    Add FinishReasonContent jelly

--- src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/output/FinishReasonContent.java
+++ src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/output/FinishReasonContent.java
@@ -6,6 +6,7 @@ import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelFinishReason;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelOutputType;
 import java.util.Set;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 public class FinishReasonContent extends ModelOutput {
@@ -26,6 +27,11 @@ public class FinishReasonContent extends ModelOutput {
         @Override
         public Set<ModelOutputType> getRequiredOutputTypes() {
             return Set.of();
+        }
+
+        @Override
+        public @NonNull String getDisplayName() {
+            return "Finish Reason Content";
         }
     }
 }
