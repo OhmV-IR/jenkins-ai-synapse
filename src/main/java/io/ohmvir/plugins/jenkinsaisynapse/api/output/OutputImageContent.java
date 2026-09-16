a516c2a5f1a177e59f294be0eff3be07f693c15f
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-15 22:01:21 -0700

    Add OutputImageContent jelly (#87)

--- src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/output/OutputImageContent.java
+++ src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/output/OutputImageContent.java
@@ -4,9 +4,15 @@ import hudson.Extension;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelCapability;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelOutputType;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.Set;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.imageio.ImageIO;
 
 public class OutputImageContent extends ModelOutput {
     private @Getter final BufferedImage image;
@@ -14,6 +20,20 @@ public class OutputImageContent extends ModelOutput {
     @DataBoundConstructor
     public OutputImageContent(BufferedImage image) {
         this.image = image;
+    }
+
+    public String getImageBase64() {
+        if (image == null) {
+            return null;
+        }
+
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            ImageIO.write(image, "png", baos);
+            byte[] bytes = baos.toByteArray();
+            return "data:image/png;base64," + Base64.getEncoder().encodeToString(bytes);
+        } catch (IOException e) {
+            return null;
+        }
     }
 
     @Extension
@@ -26,6 +46,11 @@ public class OutputImageContent extends ModelOutput {
         @Override
         public Set<ModelOutputType> getRequiredOutputTypes() {
             return Set.of(ModelOutputType.IMAGE);
+        }
+
+        @Override
+        public @NonNull String getDisplayName() {
+            return "Output Image Content";
         }
     }
 }
