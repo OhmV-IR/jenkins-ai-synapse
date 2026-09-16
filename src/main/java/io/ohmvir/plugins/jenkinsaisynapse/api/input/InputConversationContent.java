2155f954b3b30524d2108a8cfb1a91fb9265043c
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-15 22:01:35 -0700

    Add InputConversationContent jelly (#84)

--- src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/InputConversationContent.java
+++ src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/InputConversationContent.java
@@ -6,6 +6,7 @@ import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelCapability;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelInputType;
 import java.util.Set;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 public class InputConversationContent extends ModelInput {
@@ -26,6 +27,11 @@ public class InputConversationContent extends ModelInput {
         @Override
         public Set<ModelInputType> getRequiredInputTypes() {
             return Set.of();
+        }
+
+        @Override
+        public @NonNull String getDisplayName() {
+            return "Input Conversation Content";
         }
     }
 }
