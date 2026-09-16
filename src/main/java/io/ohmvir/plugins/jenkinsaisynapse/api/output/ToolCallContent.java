21e2c0960d892518d034d4434012a0eb40f65816
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-15 22:00:48 -0700

    Add ToolCallContent jelly (#95)

--- src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/output/ToolCallContent.java
+++ src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/output/ToolCallContent.java
@@ -1,23 +1,34 @@
 package io.ohmvir.plugins.jenkinsaisynapse.api.output;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import hudson.Extension;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelCapability;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelOutputType;
 import java.util.Set;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 public class ToolCallContent extends ModelOutput {
     private @Getter final String toolUseId;
     private @Getter final JsonObject toolArguments;
     private @Getter final String name;
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
     @DataBoundConstructor
     public ToolCallContent(String toolUseId, JsonObject toolArguments, String name) {
         this.toolUseId = toolUseId;
         this.toolArguments = toolArguments;
         this.name = name;
+    }
+
+    public String getArgumentsPrettyJson(){
+        if(toolArguments == null){
+            return "{}";
+        }
+        return GSON.toJson(toolArguments);
     }
 
     @Extension
@@ -30,6 +41,11 @@ public class ToolCallContent extends ModelOutput {
         @Override
         public Set<ModelOutputType> getRequiredOutputTypes() {
             return Set.of();
+        }
+
+        @Override
+        public @NonNull String getDisplayName() {
+            return "Tool Call Content";
         }
     }
 }
