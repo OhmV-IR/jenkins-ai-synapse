ea5e09cf2cd0eb24378c0685fc22639990a84c05
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-15 22:01:17 -0700

    Add InputImageContent jelly (#88)

--- src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/InputImageContent.java
+++ src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/InputImageContent.java
@@ -4,9 +4,15 @@ import hudson.Extension;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelCapability;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelInputType;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.Set;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import javax.imageio.ImageIO;
 
 public class InputImageContent extends ModelInput {
     private @Getter final BufferedImage image;
@@ -14,6 +20,19 @@ public class InputImageContent extends ModelInput {
     @DataBoundConstructor
     public InputImageContent(BufferedImage image) {
         this.image = image;
+    }
+
+    public String getImageBase64(){
+        if(image == null){
+            return null;
+        }
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            ImageIO.write(image, "png", baos);
+            byte[] bytes = baos.toByteArray();
+            return "data:image/png;base64," + Base64.getEncoder().encodeToString(bytes);
+        } catch (IOException e) {
+            return null;
+        }
     }
 
     @Extension
@@ -26,6 +45,11 @@ public class InputImageContent extends ModelInput {
         @Override
         public Set<ModelInputType> getRequiredInputTypes() {
             return Set.of(ModelInputType.IMAGE);
+        }
+
+        @Override
+        public @NonNull String getDisplayName() {
+            return "Input Image Content";
         }
     }
 }
