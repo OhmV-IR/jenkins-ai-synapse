97ee7815c48ff30c61de68eec5f2c1f8fa211600
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-15 22:01:25 -0700

    Add jelly/output video content (#86)
    * Add OutputVideoContent jelly
    
    * set displayname in java

--- src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/output/OutputVideoContent.java
+++ src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/output/OutputVideoContent.java
@@ -3,9 +3,16 @@ package io.ohmvir.plugins.jenkinsaisynapse.api.output;
 import hudson.Extension;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelCapability;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelOutputType;
+
+import java.io.IOException;
 import java.util.Set;
+
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest2;
+import org.kohsuke.stapler.StaplerResponse2;
 
 public class OutputVideoContent extends ModelOutput {
     private @Getter final byte[] videoData;
@@ -13,6 +20,16 @@ public class OutputVideoContent extends ModelOutput {
     @DataBoundConstructor
     public OutputVideoContent(byte[] videoData) {
         this.videoData = videoData;
+    }
+
+    public void doVideo(StaplerRequest2 req, StaplerResponse2 rsp) throws IOException {
+        if(videoData == null || videoData.length == 0){
+            rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        rsp.setContentType("video/mp4");
+        rsp.setContentLength(videoData.length);
+        rsp.getOutputStream().write(videoData);
     }
 
     @Extension
@@ -25,6 +42,11 @@ public class OutputVideoContent extends ModelOutput {
         @Override
         public Set<ModelOutputType> getRequiredOutputTypes() {
             return Set.of(ModelOutputType.VIDEO);
+        }
+
+        @Override
+        public @NonNull String getDisplayName() {
+            return "Output Video Content";
         }
     }
 }
