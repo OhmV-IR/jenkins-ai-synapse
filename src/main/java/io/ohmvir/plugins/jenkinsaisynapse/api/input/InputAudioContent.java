1d54064a8d044273e3e7dfc537f96a186df8980e
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-15 20:46:33 -0700

    Add InputAudioContent jelly

--- src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/InputAudioContent.java
+++ src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/InputAudioContent.java
@@ -3,9 +3,16 @@ package io.ohmvir.plugins.jenkinsaisynapse.api.input;
 import hudson.Extension;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelCapability;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelInputType;
+
+import java.io.IOException;
 import java.util.Set;
+
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest2;
+import org.kohsuke.stapler.StaplerResponse2;
 
 public class InputAudioContent extends ModelInput {
     private @Getter final byte[] audioData;
@@ -13,6 +20,16 @@ public class InputAudioContent extends ModelInput {
     @DataBoundConstructor
     public InputAudioContent(byte[] audioData) {
         this.audioData = audioData;
+    }
+
+    public void doAudio(StaplerRequest2 req, StaplerResponse2 rsp) throws IOException {
+        if(audioData == null || audioData.length == 0){
+            rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        rsp.setContentType("audio/mpeg");
+        rsp.setContentLength(audioData.length);
+        rsp.getOutputStream().write(audioData);
     }
 
     @Extension
@@ -25,6 +42,11 @@ public class InputAudioContent extends ModelInput {
         @Override
         public Set<ModelInputType> getRequiredInputTypes() {
             return Set.of(ModelInputType.AUDIO);
+        }
+
+        @Override
+        public @NonNull String getDisplayName() {
+            return "Input Audio Content";
         }
     }
 }
