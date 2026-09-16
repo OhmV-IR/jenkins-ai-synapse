b2bf8f035211585977e446b7422af10f67d1ead1
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-15 19:52:38 -0700

    Add SystemPromptContent jelly

--- src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/SystemPromptContent.java
+++ src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/SystemPromptContent.java
@@ -5,6 +5,7 @@ import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelCapability;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelInputType;
 import java.util.Set;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 public class SystemPromptContent extends ModelInput {
@@ -25,6 +26,11 @@ public class SystemPromptContent extends ModelInput {
         @Override
         public Set<ModelInputType> getRequiredInputTypes() {
             return Set.of();
+        }
+
+        @Override
+        public @NonNull String getDisplayName() {
+            return "System Prompt Content";
         }
     }
 }
