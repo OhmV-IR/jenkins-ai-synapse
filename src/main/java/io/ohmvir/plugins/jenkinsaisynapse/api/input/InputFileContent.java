519c5b215be65fb72ea18a913165ef1f6d73630d
Adrien Bourdeaux <bourdeaux.adrien@gmail.com>
2026-09-15 20:53:52 -0700

    Add InputFileContent jelly

--- src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/InputFileContent.java
+++ src/main/java/io/ohmvir/plugins/jenkinsaisynapse/api/input/InputFileContent.java
@@ -3,18 +3,41 @@ package io.ohmvir.plugins.jenkinsaisynapse.api.input;
 import hudson.Extension;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelCapability;
 import io.ohmvir.plugins.jenkinsaisynapse.api.models.ModelInputType;
+
+import java.io.IOException;
 import java.util.Set;
+
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.StaplerRequest2;
+import org.kohsuke.stapler.StaplerResponse2;
 
 public class InputFileContent extends ModelInput {
     private @Getter final String fileId;
     private @Getter final byte[] fileData;
+    private @Getter final String contentType;
 
     @DataBoundConstructor
-    public InputFileContent(String fileId, byte[] fileData) {
+    public InputFileContent(String fileId, byte[] fileData, String contentType) {
         this.fileId = fileId;
         this.fileData = fileData;
+        this.contentType = contentType;
+    }
+
+    public void doDownload(StaplerRequest2 req, StaplerResponse2 rsp) throws IOException {
+        if (fileData == null || fileData.length == 0) {
+            rsp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        rsp.setContentType(contentType);
+        rsp.setContentLength(fileData.length);
+
+        rsp.setHeader("Content-Disposition", "attachment; filename=\"" + fileId + "\"");
+
+        rsp.getOutputStream().write(fileData);
     }
 
     @Extension
@@ -27,6 +50,11 @@ public class InputFileContent extends ModelInput {
         @Override
         public Set<ModelInputType> getRequiredInputTypes() {
             return Set.of(ModelInputType.FILE);
+        }
+
+        @Override
+        public @NonNull String getDisplayName() {
+            return "Input File Content";
         }
     }
 }
